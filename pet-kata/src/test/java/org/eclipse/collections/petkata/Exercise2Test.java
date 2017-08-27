36797b527a23f00401aaec79a27439e787900ec9
Donald Raab <Donald.Raab@bnymellon.com>
2017-08-26 22:15:17 -0400

    Updated Exercise2Test for PetKata.
    Signed-off-by: Donald Raab <Donald.Raab@bnymellon.com>

--- pet-kata/src/test/java/org/eclipse/collections/petkata/Exercise2Test.java
+++ pet-kata/src/test/java/org/eclipse/collections/petkata/Exercise2Test.java
@@ -12,6 +12,7 @@ package org.eclipse.collections.petkata;
 
 import org.eclipse.collections.api.block.function.Function;
 import org.eclipse.collections.api.block.predicate.Predicate;
+import org.eclipse.collections.api.block.predicate.Predicate2;
 import org.eclipse.collections.api.list.MutableList;
 import org.eclipse.collections.api.set.MutableSet;
 import org.eclipse.collections.impl.factory.Lists;
@@ -20,12 +21,35 @@ import org.eclipse.collections.impl.test.Verify;
 import org.junit.Assert;
 import org.junit.Test;
 
+/**
+ * In the slides leading up to this exercise you should have learned about<br>
+ * {@link MutableList#flatCollect(Function)}<br>
+ * {@link MutableList#select(Predicate)}<br>
+ * {@link MutableList#reject(Predicate)}<br>
+ * {@link MutableList#count(Predicate)}<br>
+ * {@link MutableList#detect(Predicate)}<br>
+ * {@link MutableList#anySatisfy(Predicate)}<br>
+ * {@link MutableList#allSatisfy(Predicate)}<br>
+ * {@link MutableList#noneSatisfy(Predicate)}<br>
+ * <br>
+ * as well as the corresponding with methods<br>
+ * <br>
+ * {@link MutableList#selectWith(Predicate2, Object)}<br>
+ * {@link MutableList#rejectWith(Predicate2, Object)}<br>
+ * {@link MutableList#countWith(Predicate2, Object)}<br>
+ * {@link MutableList#detectWith(Predicate2, Object)}<br>
+ * {@link MutableList#anySatisfyWith(Predicate2, Object)}<br>
+ * {@link MutableList#allSatisfyWith(Predicate2, Object)}<br>
+ * {@link MutableList#noneSatisfyWith(Predicate2, Object)}<br>
+ * <br>
+ * @see <a href="http://eclipse.github.io/eclipse-collections-kata/pet-kata/#/4">Exercise 2 Slides</a>
+ */
 public class Exercise2Test extends PetDomainForKata
 {
     @Test
     public void doAnyPeopleHaveCats()
     {
-        Predicate<Person> predicate = null; //replace null with a predicate which checks for PetType.CAT
+        Predicate<Person> predicate = null; //replace null with a Predicate lambda which checks for PetType.CAT
         Assert.assertTrue(this.people.anySatisfy(predicate));
     }
 
@@ -33,7 +57,7 @@ public class Exercise2Test extends PetDomainForKata
     public void doAllPeopleHavePets()
     {
         Predicate<Person> predicate = person -> person.isPetPerson();
-        boolean result = true; //replace with something that checks if all people have pets
+        boolean result = true; //replace with a method call send to this.people that checks if all people have pets
         Assert.assertFalse(result);
     }
 
@@ -55,7 +79,7 @@ public class Exercise2Test extends PetDomainForKata
     @Test
     public void getPeopleWithPets()
     {
-        MutableList<Person> petPeople = this.people; //replace with only the pet owners
+        MutableList<Person> petPeople = this.people; // replace with only the pet owners
         Verify.assertSize(7, petPeople);
     }
 
@@ -72,7 +96,7 @@ public class Exercise2Test extends PetDomainForKata
     @Test
     public void getFirstNamesOfAllPeople()
     {
-        MutableList<String> firstNames = null;
+        MutableList<String> firstNames = null;  // Transform this.people into a list of first names
         Assert.assertEquals(
                 Lists.mutable.with("Mary", "Bob", "Ted", "Jake", "Barry", "Terry", "Harry", "John"),
                 firstNames);

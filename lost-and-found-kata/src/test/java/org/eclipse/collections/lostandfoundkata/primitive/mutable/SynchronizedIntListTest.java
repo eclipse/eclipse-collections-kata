d94bbcfaade8c2b58a393b156a58f77abda33497
Donald Raab <Donald.Raab@bnymellon.com>
2021-09-13 01:22:16 -0400

    Add SynchronizedIntListTest to Lost and Found Kata.
    Signed-off-by: Donald Raab <Donald.Raab@bnymellon.com>

--- lost-and-found-kata/src/test/java/org/eclipse/collections/lostandfoundkata/primitive/mutable/SynchronizedIntListTest.java
+++ lost-and-found-kata/src/test/java/org/eclipse/collections/lostandfoundkata/primitive/mutable/SynchronizedIntListTest.java
@@ -11,10 +11,27 @@
 package org.eclipse.collections.lostandfoundkata.primitive.mutable;
 
 import java.util.IntSummaryStatistics;
+import java.util.function.Consumer;
 import java.util.function.IntConsumer;
 import java.util.function.IntSupplier;
 
 import org.eclipse.collections.api.bag.primitive.MutableIntBag;
+import org.eclipse.collections.api.block.function.primitive.IntToBooleanFunction;
+import org.eclipse.collections.api.block.function.primitive.IntToByteFunction;
+import org.eclipse.collections.api.block.function.primitive.IntToCharFunction;
+import org.eclipse.collections.api.block.function.primitive.IntToDoubleFunction;
+import org.eclipse.collections.api.block.function.primitive.IntToFloatFunction;
+import org.eclipse.collections.api.block.function.primitive.IntToLongFunction;
+import org.eclipse.collections.api.block.function.primitive.IntToObjectFunction;
+import org.eclipse.collections.api.block.function.primitive.IntToShortFunction;
+import org.eclipse.collections.api.block.predicate.primitive.IntPredicate;
+import org.eclipse.collections.api.collection.primitive.MutableBooleanCollection;
+import org.eclipse.collections.api.collection.primitive.MutableByteCollection;
+import org.eclipse.collections.api.collection.primitive.MutableCharCollection;
+import org.eclipse.collections.api.collection.primitive.MutableDoubleCollection;
+import org.eclipse.collections.api.collection.primitive.MutableFloatCollection;
+import org.eclipse.collections.api.collection.primitive.MutableLongCollection;
+import org.eclipse.collections.api.collection.primitive.MutableShortCollection;
 import org.eclipse.collections.api.list.MutableList;
 import org.eclipse.collections.api.list.primitive.BooleanList;
 import org.eclipse.collections.api.list.primitive.ByteList;
@@ -36,6 +53,7 @@ import org.eclipse.collections.impl.factory.primitive.IntLists;
 import org.eclipse.collections.impl.factory.primitive.IntSets;
 import org.eclipse.collections.impl.factory.primitive.LongLists;
 import org.eclipse.collections.impl.factory.primitive.ShortLists;
+import org.eclipse.collections.impl.list.Interval;
 import org.eclipse.collections.impl.list.primitive.IntInterval;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -46,117 +64,181 @@ public class SynchronizedIntListTest
 {
     private MutableIntList synchList;
 
+    /**
+     * {@link MutableIntList#asSynchronized()}
+     */
     @BeforeEach
     void setUp()
     {
-        this.synchList = IntLists.mutable.with(1, 2, 3, 4, 5).asSynchronized();
+        // Make this IntList thread-safe by calling asSynchronized
+        this.synchList = IntLists.mutable.with(1, 2, 3, 4, 5);
     }
 
+    /**
+     * Change the synchList initialization code in the setUp method to get this test to pass.
+     * <p>
+     * {@link MutableIntList#add(int)} <br>
+     * {@link MutableIntList#size()} <br>
+     */
     @Test
-    @Tag("SOLUTION")
     public void isThreadSafe()
     {
         this.synchList.clear();
         this.assertThreadSafe(this.synchList::add, this.synchList::size);
     }
 
+    /**
+     * {@link IntInterval#oneTo(int)} <br>
+     * {@link IntInterval#primitiveParallelStream()} <br>
+     * {@link java.util.stream.IntStream#forEach(IntConsumer)} <br>
+     * {@link IntSupplier#getAsInt()} <br>
+     */
     private void assertThreadSafe(IntConsumer consumer, IntSupplier supplier)
     {
         IntInterval.oneTo(1000).primitiveParallelStream().forEach(consumer);
         Assertions.assertEquals(1000, supplier.getAsInt());
     }
 
+    /**
+     * {@link MutableIntList#toList()}
+     * {@link MutableIntList#toSet()}
+     * {@link MutableIntList#toBag()}
+     */
     @Test
-    @Tag("SOLUTION")
     public void converting()
     {
-        MutableIntList list = this.synchList.toList();
+        // Convert this.synchList to a MutableIntList
+        MutableIntList list = null;
         Assertions.assertEquals(IntLists.mutable.with(1, 2, 3, 4, 5), list);
 
-        MutableIntSet set = this.synchList.toSet();
+        // Convert this.synchList to a MutableIntSet
+        MutableIntSet set = null;
         Assertions.assertEquals(IntSets.mutable.with(1, 2, 3, 4, 5), set);
 
-        MutableIntBag bag = this.synchList.toBag();
+        // Convert this.synchList to a MutableIntBag
+        MutableIntBag bag = null;
         Assertions.assertEquals(IntBags.mutable.with(1, 2, 3, 4, 5), bag);
     }
 
+    /**
+     * Inclusive Filter: {@link MutableIntList#select(IntPredicate)} <br>
+     * Exclusive Filter: {@link MutableIntList#reject(IntPredicate)} <br>
+     */
     @Test
-    @Tag("SOLUTION")
     public void filtering()
     {
-        // Inclusive
-        MutableIntList select = this.synchList.select(each -> each % 2 == 0);
+        IntPredicate isEven = each -> each % 2 == 0;
+        // Filter the list inclusively based on the isEven predicate
+        MutableIntList select = this.synchList;
         Assertions.assertEquals(IntInterval.evensFromTo(2, 4), select);
 
-        // Exclusive
-        MutableIntList reject = this.synchList.reject(each -> each % 2 == 0);  // Exclusive
+        // Filter the list exclusively based on the isEven predicate
+        MutableIntList reject = this.synchList;
         Assertions.assertEquals(IntInterval.oddsFromTo(1, 5), reject);
     }
 
+    /**
+     * {@link MutableIntList#collectBoolean(IntToBooleanFunction, MutableBooleanCollection)} <br>
+     * {@link MutableIntList#collectByte(IntToByteFunction, MutableByteCollection)} <br>
+     * {@link MutableIntList#collectChar(IntToCharFunction, MutableCharCollection)} <br>
+     * {@link MutableIntList#collectShort(IntToShortFunction, MutableShortCollection)} <br>
+     * {@link MutableIntList#collectFloat(IntToFloatFunction, MutableFloatCollection)} <br>
+     * {@link MutableIntList#collectLong(IntToLongFunction, MutableLongCollection)} <br>
+     * {@link MutableIntList#collectDouble(IntToDoubleFunction, MutableDoubleCollection)} <br>
+     * {@link MutableIntList#collect(IntToObjectFunction)} <br>
+     */
     @Test
-    @Tag("SOLUTION")
     public void transforming()
     {
         // Type Transformations using fused methods with target collections
-        BooleanList booleanList = this.synchList.collectBoolean(each -> each % 2 == 0, BooleanLists.mutable.empty());
+        // Collect to MutableBooleanList evens=true, odds=false
+        BooleanList booleanList = null;
         var expectedBooleanList = BooleanLists.mutable.with(false, true, false, true, false);
         Assertions.assertEquals(expectedBooleanList, booleanList);
 
-        ByteList byteList = this.synchList.collectByte(each -> (byte) each, ByteLists.mutable.empty());
+        // Collect to MutableByteList the values as is
+        ByteList byteList = null;
         var expectedByteList = ByteLists.mutable.with((byte) 1, (byte) 2, (byte) 3, (byte) 4, (byte) 5);
         Assertions.assertEquals(expectedByteList, byteList);
         long byteSum = byteList.sum();
         Assertions.assertEquals(15L, byteSum);
 
-        CharList charList = this.synchList.collectChar(each -> (char) each, CharLists.mutable.empty());
+        // Collect to MutableCharList the values as is
+        CharList charList = null;
         var expectedCharList = CharLists.mutable.with((char) 1, (char) 2, (char) 3, (char) 4, (char) 5);
         Assertions.assertEquals(expectedCharList, charList);
         long charSum = charList.sum();
         Assertions.assertEquals(15L, charSum);
 
-        ShortList shortList = this.synchList.collectShort(each -> (short) each, ShortLists.mutable.empty());
+        // Collect to MutableShortList the values as is
+        ShortList shortList = null;
         var expectedShortList = ShortLists.mutable.with((short) 1, (short) 2, (short) 3, (short) 4, (short) 5);
         Assertions.assertEquals(expectedShortList, shortList);
         long shortSum = shortList.sum();
         Assertions.assertEquals(15L, shortSum);
 
-        FloatList floatList = this.synchList.collectFloat(each -> (float) each, FloatLists.mutable.empty());
+        // Collect to MutableFloatList the values as is
+        FloatList floatList = null;
         var expectedFloatList = FloatLists.mutable.with(1.0f, 2.0f, 3.0f, 4.0f, 5.0f);
         Assertions.assertEquals(expectedFloatList, floatList);
         double floatSum = floatList.sum();
         Assertions.assertEquals(15.0, floatSum, 0.0);
 
-        LongList longList = this.synchList.collectLong(each -> (long) each, LongLists.mutable.empty());
+        // Collect to MutableLongList the values as is
+        LongList longList = null;
         var expectedLongList = LongLists.mutable.with(1L, 2L, 3L, 4L, 5L);
         Assertions.assertEquals(expectedLongList, longList);
         long longSum = longList.sum();
         Assertions.assertEquals(15L, longSum);
 
-        DoubleList doubleList = this.synchList.collectDouble(each -> (double) each, DoubleLists.mutable.empty());
+        // Collect to MutableDoubleList the values as is
+        DoubleList doubleList = null;
         var expectedDoubleList = DoubleLists.mutable.with(1.0, 2.0, 3.0, 4.0, 5.0);
         Assertions.assertEquals(expectedDoubleList, doubleList.toList());
         double doubleSum = doubleList.sum();
         Assertions.assertEquals(15.0, doubleSum, 0.0);
 
-        MutableList<String> stringList = this.synchList.collect(Integer::toString);
+        // Collect to MutableList of String the values as Strings
+        MutableList<String> stringList = null;
         var expectedStringList = Lists.mutable.with("1", "2", "3", "4", "5");
         Assertions.assertEquals(expectedStringList, stringList);
     }
 
+    /**
+     * {@link MutableIntList#sum()} <br>
+     * {@link MutableIntList#max()} <br>
+     * {@link MutableIntList#maxIfEmpty(int)} <br>
+     * {@link MutableIntList#min()} <br>
+     * {@link MutableIntList#minIfEmpty(int)} <br>
+     * {@link MutableIntList#average()} <br>
+     * {@link MutableIntList#averageIfEmpty(double)} <br>
+     * {@link MutableIntList#median()} <br>
+     * {@link MutableIntList#medianIfEmpty(double)} <br>
+     * {@link MutableIntList#summaryStatistics()} <br>
+     */
     @Test
-    @Tag("SOLUTION")
     public void calculating()
     {
-        long sum = this.synchList.sum();
-        int max = this.synchList.max();
-        int maxIfEmpty = this.synchList.maxIfEmpty(0);
-        int min = this.synchList.min();
-        int minIfEmpty = this.synchList.minIfEmpty(0);
-        double average = this.synchList.average();
-        double averageIfEmpty = this.synchList.averageIfEmpty(0.0);
-        double median = this.synchList.median();
-        double medianIfEmpty = this.synchList.medianIfEmpty(0.0);
-        IntSummaryStatistics stats = this.synchList.summaryStatistics();
+        // Calculate the sum of this.syncList
+        long sum = 0L;
+        // Calculate the max of this.syncList
+        int max = 0;
+        // Calculate the max of this.syncList, and 0 if empty
+        int maxIfEmpty = 0;
+        // Calculate the min of this.syncList
+        int min = 0;
+        // Calculate the min of this.syncList, and 0 if empty
+        int minIfEmpty = 0;
+        // Calculate the average of this.syncList
+        double average = 0.0;
+        // Calculate the average of this.syncList, and 0.0 if empty
+        double averageIfEmpty = 0.0;
+        // Calculate the median of this.syncList
+        double median = 0.0;
+        // Calculate the median of this.syncList, and 0.0 if empty
+        double medianIfEmpty = 0.0;
+        // Calculate the summaryStatistics of this.syncList
+        IntSummaryStatistics stats = new IntSummaryStatistics();
 
         Assertions.assertEquals(15, sum);
         Assertions.assertEquals(1, min);

7e137de8496508822f289153e41ddc796f3001eb
Donald Raab <Donald.Raab@bnymellon.com>
2022-03-16 17:03:50 -0400

    Refactor primitive synchronized List tests in lost-and-found-kata.
    Signed-off-by: Donald Raab <Donald.Raab@bnymellon.com>

--- lost-and-found-kata-solutions/src/test/java/org/eclipse/collections/lostandfoundkata/primitive/mutable/SynchronizedIntListTest.java
+++ lost-and-found-kata-solutions/src/test/java/org/eclipse/collections/lostandfoundkata/primitive/mutable/SynchronizedIntListTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021 The Bank of New York Mellon.
+ * Copyright (c) 2022 The Bank of New York Mellon.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * and Eclipse Distribution License v. 1.0 which accompany this distribution.
@@ -68,26 +68,41 @@ public class SynchronizedIntListTest
 
     @Test
     @Tag("SOLUTION")
-    public void converting()
+    public void toList()
     {
         MutableIntList list = this.synchList.toList();
         Assertions.assertEquals(IntLists.mutable.with(1, 2, 3, 4, 5), list);
+    }
 
+    @Test
+    @Tag("SOLUTION")
+    public void toSet()
+    {
         MutableIntSet set = this.synchList.toSet();
         Assertions.assertEquals(IntSets.mutable.with(1, 2, 3, 4, 5), set);
+    }
 
+    @Test
+    @Tag("SOLUTION")
+    public void toBag()
+    {
         MutableIntBag bag = this.synchList.toBag();
         Assertions.assertEquals(IntBags.mutable.with(1, 2, 3, 4, 5), bag);
     }
 
     @Test
     @Tag("SOLUTION")
-    public void filtering()
+    public void select()
     {
         // Inclusive
         MutableIntList select = this.synchList.select(each -> each % 2 == 0);
         Assertions.assertEquals(IntInterval.evensFromTo(2, 4), select);
+    }
 
+    @Test
+    @Tag("SOLUTION")
+    public void reject()
+    {
         // Exclusive
         MutableIntList reject = this.synchList.reject(each -> each % 2 == 0);  // Exclusive
         Assertions.assertEquals(IntInterval.oddsFromTo(1, 5), reject);
@@ -95,49 +110,83 @@ public class SynchronizedIntListTest
 
     @Test
     @Tag("SOLUTION")
-    public void transforming()
+    public void collectBoolean()
     {
-        // Type Transformations using fused methods with target collections
         BooleanList booleanList = this.synchList.collectBoolean(each -> each % 2 == 0, BooleanLists.mutable.empty());
         var expectedBooleanList = BooleanLists.mutable.with(false, true, false, true, false);
         Assertions.assertEquals(expectedBooleanList, booleanList);
+    }
 
+    @Test
+    @Tag("SOLUTION")
+    public void collectByte()
+    {
         ByteList byteList = this.synchList.collectByte(each -> (byte) each, ByteLists.mutable.empty());
         var expectedByteList = ByteLists.mutable.with((byte) 1, (byte) 2, (byte) 3, (byte) 4, (byte) 5);
         Assertions.assertEquals(expectedByteList, byteList);
         long byteSum = byteList.sum();
         Assertions.assertEquals(15L, byteSum);
+    }
 
+    @Test
+    @Tag("SOLUTION")
+    public void collectChar()
+    {
         CharList charList = this.synchList.collectChar(each -> (char) each, CharLists.mutable.empty());
         var expectedCharList = CharLists.mutable.with((char) 1, (char) 2, (char) 3, (char) 4, (char) 5);
         Assertions.assertEquals(expectedCharList, charList);
         long charSum = charList.sum();
         Assertions.assertEquals(15L, charSum);
+    }
 
+    @Test
+    @Tag("SOLUTION")
+    public void collectShort()
+    {
         ShortList shortList = this.synchList.collectShort(each -> (short) each, ShortLists.mutable.empty());
         var expectedShortList = ShortLists.mutable.with((short) 1, (short) 2, (short) 3, (short) 4, (short) 5);
         Assertions.assertEquals(expectedShortList, shortList);
         long shortSum = shortList.sum();
         Assertions.assertEquals(15L, shortSum);
+    }
 
+    @Test
+    @Tag("SOLUTION")
+    public void collectFloat()
+    {
         FloatList floatList = this.synchList.collectFloat(each -> (float) each, FloatLists.mutable.empty());
         var expectedFloatList = FloatLists.mutable.with(1.0f, 2.0f, 3.0f, 4.0f, 5.0f);
         Assertions.assertEquals(expectedFloatList, floatList);
         double floatSum = floatList.sum();
         Assertions.assertEquals(15.0, floatSum, 0.0);
+    }
 
+    @Test
+    @Tag("SOLUTION")
+    public void collectLong()
+    {
         LongList longList = this.synchList.collectLong(each -> (long) each, LongLists.mutable.empty());
         var expectedLongList = LongLists.mutable.with(1L, 2L, 3L, 4L, 5L);
         Assertions.assertEquals(expectedLongList, longList);
         long longSum = longList.sum();
         Assertions.assertEquals(15L, longSum);
+    }
 
+    @Test
+    @Tag("SOLUTION")
+    public void collectDouble()
+    {
         DoubleList doubleList = this.synchList.collectDouble(each -> (double) each, DoubleLists.mutable.empty());
         var expectedDoubleList = DoubleLists.mutable.with(1.0, 2.0, 3.0, 4.0, 5.0);
-        Assertions.assertEquals(expectedDoubleList, doubleList.toList());
+        Assertions.assertEquals(expectedDoubleList, doubleList);
         double doubleSum = doubleList.sum();
         Assertions.assertEquals(15.0, doubleSum, 0.0);
+    }
 
+    @Test
+    @Tag("SOLUTION")
+    public void collect()
+    {
         MutableList<String> stringList = this.synchList.collect(Integer::toString);
         var expectedStringList = Lists.mutable.with("1", "2", "3", "4", "5");
         Assertions.assertEquals(expectedStringList, stringList);
@@ -145,32 +194,85 @@ public class SynchronizedIntListTest
 
     @Test
     @Tag("SOLUTION")
-    public void calculating()
+    public void sum()
     {
         long sum = this.synchList.sum();
-        int max = this.synchList.max();
-        int maxIfEmpty = this.synchList.maxIfEmpty(0);
-        int min = this.synchList.min();
-        int minIfEmpty = this.synchList.minIfEmpty(0);
-        double average = this.synchList.average();
-        double averageIfEmpty = this.synchList.averageIfEmpty(0.0);
-        double median = this.synchList.median();
-        double medianIfEmpty = this.synchList.medianIfEmpty(0.0);
-        IntSummaryStatistics stats = this.synchList.summaryStatistics();
-
         Assertions.assertEquals(15, sum);
-        Assertions.assertEquals(1, min);
-        Assertions.assertEquals(min, minIfEmpty);
+    }
+
+    @Test
+    @Tag("SOLUTION")
+    public void max()
+    {
+        int max = this.synchList.max();
         Assertions.assertEquals(5, max);
-        Assertions.assertEquals(max, maxIfEmpty);
+    }
+
+    @Test
+    @Tag("SOLUTION")
+    public void maxIfEmpty()
+    {
+        int maxIfEmpty = this.synchList.maxIfEmpty(0);
+        Assertions.assertEquals(5, maxIfEmpty);
+    }
+
+    @Test
+    @Tag("SOLUTION")
+    public void min()
+    {
+        int min = this.synchList.min();
+        Assertions.assertEquals(1, min);
+    }
+
+    @Test
+    @Tag("SOLUTION")
+    public void minIfEmpty()
+    {
+        int minIfEmpty = this.synchList.minIfEmpty(0);
+        Assertions.assertEquals(1, minIfEmpty);
+    }
+
+    @Test
+    @Tag("SOLUTION")
+    public void average()
+    {
+        double average = this.synchList.average();
         Assertions.assertEquals(3.0, average, 0.0);
-        Assertions.assertEquals(average, averageIfEmpty, 0.0);
+    }
+
+    @Test
+    @Tag("SOLUTION")
+    public void averageIfEmpty()
+    {
+        double averageIfEmpty = this.synchList.averageIfEmpty(0.0);
+        Assertions.assertEquals(3.0, averageIfEmpty, 0.0);
+    }
+
+    @Test
+    @Tag("SOLUTION")
+    public void median()
+    {
+        double median = this.synchList.median();
         Assertions.assertEquals(3.0, median, 0.0);
-        Assertions.assertEquals(median, medianIfEmpty, 0.0);
-        Assertions.assertEquals(sum, stats.getSum());
-        Assertions.assertEquals(min, stats.getMin());
-        Assertions.assertEquals(max, stats.getMax());
-        Assertions.assertEquals(average, stats.getAverage(), 0.0);
+    }
+
+    @Test
+    @Tag("SOLUTION")
+    public void medianIfEmpty()
+    {
+        double medianIfEmpty = this.synchList.medianIfEmpty(0.0);
+        Assertions.assertEquals(3.0, medianIfEmpty, 0.0);
+    }
+
+    @Test
+    @Tag("SOLUTION")
+    public void summaryStatistics()
+    {
+        IntSummaryStatistics stats = this.synchList.summaryStatistics();
+        Assertions.assertEquals(15L, stats.getSum());
+        Assertions.assertEquals(1, stats.getMin());
+        Assertions.assertEquals(5, stats.getMax());
+        Assertions.assertEquals(3.0, stats.getAverage(), 0.0);
         Assertions.assertEquals(this.synchList.size(), stats.getCount());
     }
 }
--- lost-and-found-kata/src/test/java/org/eclipse/collections/lostandfoundkata/primitive/mutable/SynchronizedIntListTest.java
+++ lost-and-found-kata/src/test/java/org/eclipse/collections/lostandfoundkata/primitive/mutable/SynchronizedIntListTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021 The Bank of New York Mellon.
+ * Copyright (c) 2022 The Bank of New York Mellon.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * and Eclipse Distribution License v. 1.0 which accompany this distribution.
@@ -60,13 +60,52 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
+/**
+ * {@link MutableIntList#asSynchronized()} <br>
+ * <p/>
+ * {@link MutableIntList#add(int)} <br>
+ * {@link MutableIntList#size()} <br>
+ * <p/>
+ * {@link IntInterval#oneTo(int)} <br>
+ * {@link IntInterval#primitiveParallelStream()} <br>
+ * {@link java.util.stream.IntStream#forEach(IntConsumer)} <br>
+ * {@link IntSupplier#getAsInt()} <br>
+ * <p/>
+ * Converting: <br>
+ * {@link MutableIntList#toList()} <br>
+ * {@link MutableIntList#toSet()} <br>
+ * {@link MutableIntList#toBag()} <br>
+ * <p/>
+ * Filtering: <br>
+ * {@link MutableIntList#select(IntPredicate)} <br>
+ * {@link MutableIntList#reject(IntPredicate)} <br>
+ * <p/>
+ * Converting: <br>
+ * {@link MutableIntList#collectBoolean(IntToBooleanFunction, MutableBooleanCollection)} <br>
+ * {@link MutableIntList#collectByte(IntToByteFunction, MutableByteCollection)} <br>
+ * {@link MutableIntList#collectChar(IntToCharFunction, MutableCharCollection)} <br>
+ * {@link MutableIntList#collectShort(IntToShortFunction, MutableShortCollection)} <br>
+ * {@link MutableIntList#collectFloat(IntToFloatFunction, MutableFloatCollection)} <br>
+ * {@link MutableIntList#collectLong(IntToLongFunction, MutableLongCollection)} <br>
+ * {@link MutableIntList#collectDouble(IntToDoubleFunction, MutableDoubleCollection)} <br>
+ * {@link MutableIntList#collect(IntToObjectFunction)} <br>
+ * <p/>
+ * Calculating: <br>
+ * {@link MutableIntList#sum()} <br>
+ * {@link MutableIntList#max()} <br>
+ * {@link MutableIntList#maxIfEmpty(int)} <br>
+ * {@link MutableIntList#min()} <br>
+ * {@link MutableIntList#minIfEmpty(int)} <br>
+ * {@link MutableIntList#average()} <br>
+ * {@link MutableIntList#averageIfEmpty(double)} <br>
+ * {@link MutableIntList#median()} <br>
+ * {@link MutableIntList#medianIfEmpty(double)} <br>
+ * {@link MutableIntList#summaryStatistics()} <br>
+ */
 public class SynchronizedIntListTest
 {
     private MutableIntList synchList;
 
-    /**
-     * {@link MutableIntList#asSynchronized()}
-     */
     @BeforeEach
     void setUp()
     {
@@ -76,9 +115,6 @@ public class SynchronizedIntListTest
 
     /**
      * Change the synchList initialization code in the setUp method to get this test to pass.
-     * <p>
-     * {@link MutableIntList#add(int)} <br>
-     * {@link MutableIntList#size()} <br>
      */
     @Test
     public void isThreadSafe()
@@ -87,172 +123,219 @@ public class SynchronizedIntListTest
         this.assertThreadSafe(this.synchList::add, this.synchList::size);
     }
 
-    /**
-     * {@link IntInterval#oneTo(int)} <br>
-     * {@link IntInterval#primitiveParallelStream()} <br>
-     * {@link java.util.stream.IntStream#forEach(IntConsumer)} <br>
-     * {@link IntSupplier#getAsInt()} <br>
-     */
-    private void assertThreadSafe(IntConsumer consumer, IntSupplier supplier)
+    public void assertThreadSafe(IntConsumer consumer, IntSupplier supplier)
     {
         IntInterval.oneTo(1000).primitiveParallelStream().forEach(consumer);
         Assertions.assertEquals(1000, supplier.getAsInt());
     }
 
-    /**
-     * {@link MutableIntList#toList()}
-     * {@link MutableIntList#toSet()}
-     * {@link MutableIntList#toBag()}
-     */
     @Test
-    public void converting()
+    public void toList()
     {
         // Convert this.synchList to a MutableIntList
         MutableIntList list = null;
         Assertions.assertEquals(IntLists.mutable.with(1, 2, 3, 4, 5), list);
+    }
 
+    @Test
+    public void toSet()
+    {
         // Convert this.synchList to a MutableIntSet
         MutableIntSet set = null;
         Assertions.assertEquals(IntSets.mutable.with(1, 2, 3, 4, 5), set);
+    }
 
+    @Test
+    public void toBag()
+    {
         // Convert this.synchList to a MutableIntBag
         MutableIntBag bag = null;
         Assertions.assertEquals(IntBags.mutable.with(1, 2, 3, 4, 5), bag);
     }
 
-    /**
-     * Inclusive Filter: {@link MutableIntList#select(IntPredicate)} <br>
-     * Exclusive Filter: {@link MutableIntList#reject(IntPredicate)} <br>
-     */
     @Test
-    public void filtering()
+    public void select()
     {
         IntPredicate isEven = each -> each % 2 == 0;
         // Filter the list inclusively based on the isEven predicate
         MutableIntList select = this.synchList;
         Assertions.assertEquals(IntInterval.evensFromTo(2, 4), select);
+    }
 
+    @Test
+    public void reject()
+    {
+        IntPredicate isEven = each -> each % 2 == 0;
         // Filter the list exclusively based on the isEven predicate
         MutableIntList reject = this.synchList;
         Assertions.assertEquals(IntInterval.oddsFromTo(1, 5), reject);
     }
 
-    /**
-     * {@link MutableIntList#collectBoolean(IntToBooleanFunction, MutableBooleanCollection)} <br>
-     * {@link MutableIntList#collectByte(IntToByteFunction, MutableByteCollection)} <br>
-     * {@link MutableIntList#collectChar(IntToCharFunction, MutableCharCollection)} <br>
-     * {@link MutableIntList#collectShort(IntToShortFunction, MutableShortCollection)} <br>
-     * {@link MutableIntList#collectFloat(IntToFloatFunction, MutableFloatCollection)} <br>
-     * {@link MutableIntList#collectLong(IntToLongFunction, MutableLongCollection)} <br>
-     * {@link MutableIntList#collectDouble(IntToDoubleFunction, MutableDoubleCollection)} <br>
-     * {@link MutableIntList#collect(IntToObjectFunction)} <br>
-     */
     @Test
-    public void transforming()
+    public void collectBoolean()
     {
-        // Type Transformations using fused methods with target collections
         // Collect to MutableBooleanList evens=true, odds=false
         BooleanList booleanList = null;
         var expectedBooleanList = BooleanLists.mutable.with(false, true, false, true, false);
         Assertions.assertEquals(expectedBooleanList, booleanList);
+    }
 
+    @Test
+    public void collectByte()
+    {
         // Collect to MutableByteList the values as is
         ByteList byteList = null;
         var expectedByteList = ByteLists.mutable.with((byte) 1, (byte) 2, (byte) 3, (byte) 4, (byte) 5);
         Assertions.assertEquals(expectedByteList, byteList);
         long byteSum = byteList.sum();
         Assertions.assertEquals(15L, byteSum);
+    }
 
+    @Test
+    public void collectChar()
+    {
         // Collect to MutableCharList the values as is
         CharList charList = null;
         var expectedCharList = CharLists.mutable.with((char) 1, (char) 2, (char) 3, (char) 4, (char) 5);
         Assertions.assertEquals(expectedCharList, charList);
         long charSum = charList.sum();
         Assertions.assertEquals(15L, charSum);
+    }
 
+    @Test
+    public void collectShort()
+    {
         // Collect to MutableShortList the values as is
         ShortList shortList = null;
         var expectedShortList = ShortLists.mutable.with((short) 1, (short) 2, (short) 3, (short) 4, (short) 5);
         Assertions.assertEquals(expectedShortList, shortList);
         long shortSum = shortList.sum();
         Assertions.assertEquals(15L, shortSum);
+    }
 
+    @Test
+    public void collectFloat()
+    {
         // Collect to MutableFloatList the values as is
         FloatList floatList = null;
         var expectedFloatList = FloatLists.mutable.with(1.0f, 2.0f, 3.0f, 4.0f, 5.0f);
         Assertions.assertEquals(expectedFloatList, floatList);
         double floatSum = floatList.sum();
         Assertions.assertEquals(15.0, floatSum, 0.0);
+    }
 
+    @Test
+    public void collectLong()
+    {
         // Collect to MutableLongList the values as is
         LongList longList = null;
         var expectedLongList = LongLists.mutable.with(1L, 2L, 3L, 4L, 5L);
         Assertions.assertEquals(expectedLongList, longList);
         long longSum = longList.sum();
         Assertions.assertEquals(15L, longSum);
+    }
 
+    @Test
+    public void collectDouble()
+    {
         // Collect to MutableDoubleList the values as is
         DoubleList doubleList = null;
         var expectedDoubleList = DoubleLists.mutable.with(1.0, 2.0, 3.0, 4.0, 5.0);
-        Assertions.assertEquals(expectedDoubleList, doubleList.toList());
+        Assertions.assertEquals(expectedDoubleList, doubleList);
         double doubleSum = doubleList.sum();
         Assertions.assertEquals(15.0, doubleSum, 0.0);
+    }
 
+    @Test
+    public void collect()
+    {
         // Collect to MutableList of String the values as Strings
         MutableList<String> stringList = null;
         var expectedStringList = Lists.mutable.with("1", "2", "3", "4", "5");
         Assertions.assertEquals(expectedStringList, stringList);
     }
 
-    /**
-     * {@link MutableIntList#sum()} <br>
-     * {@link MutableIntList#max()} <br>
-     * {@link MutableIntList#maxIfEmpty(int)} <br>
-     * {@link MutableIntList#min()} <br>
-     * {@link MutableIntList#minIfEmpty(int)} <br>
-     * {@link MutableIntList#average()} <br>
-     * {@link MutableIntList#averageIfEmpty(double)} <br>
-     * {@link MutableIntList#median()} <br>
-     * {@link MutableIntList#medianIfEmpty(double)} <br>
-     * {@link MutableIntList#summaryStatistics()} <br>
-     */
     @Test
-    public void calculating()
+    public void sum()
     {
         // Calculate the sum of this.syncList
         long sum = 0L;
+        Assertions.assertEquals(15, sum);
+    }
+
+    @Test
+    public void max()
+    {
         // Calculate the max of this.syncList
         int max = 0;
+        Assertions.assertEquals(5, max);
+    }
+
+    @Test
+    public void maxIfEmpty()
+    {
         // Calculate the max of this.syncList, and 0 if empty
         int maxIfEmpty = 0;
+        Assertions.assertEquals(5, maxIfEmpty);
+    }
+
+    @Test
+    public void min()
+    {
         // Calculate the min of this.syncList
         int min = 0;
+        Assertions.assertEquals(1, min);
+    }
+
+    @Test
+    public void minIfEmpty()
+    {
         // Calculate the min of this.syncList, and 0 if empty
         int minIfEmpty = 0;
+        Assertions.assertEquals(1, minIfEmpty);
+    }
+
+    @Test
+    public void average()
+    {
         // Calculate the average of this.syncList
         double average = 0.0;
+        Assertions.assertEquals(3.0, average, 0.0);
+    }
+
+    @Test
+    public void averageIfEmpty()
+    {
         // Calculate the average of this.syncList, and 0.0 if empty
         double averageIfEmpty = 0.0;
+        Assertions.assertEquals(3.0, averageIfEmpty, 0.0);
+    }
+
+    @Test
+    public void median()
+    {
         // Calculate the median of this.syncList
         double median = 0.0;
+        Assertions.assertEquals(3.0, median, 0.0);
+    }
+
+    @Test
+    public void medianIfEmpty()
+    {
         // Calculate the median of this.syncList, and 0.0 if empty
         double medianIfEmpty = 0.0;
+        Assertions.assertEquals(3.0, medianIfEmpty, 0.0);
+    }
+
+    @Test
+    public void summaryStatistics()
+    {
         // Calculate the summaryStatistics of this.syncList
         IntSummaryStatistics stats = new IntSummaryStatistics();
-
-        Assertions.assertEquals(15, sum);
-        Assertions.assertEquals(1, min);
-        Assertions.assertEquals(min, minIfEmpty);
-        Assertions.assertEquals(5, max);
-        Assertions.assertEquals(max, maxIfEmpty);
-        Assertions.assertEquals(3.0, average, 0.0);
-        Assertions.assertEquals(average, averageIfEmpty, 0.0);
-        Assertions.assertEquals(3.0, median, 0.0);
-        Assertions.assertEquals(median, medianIfEmpty, 0.0);
-        Assertions.assertEquals(sum, stats.getSum());
-        Assertions.assertEquals(min, stats.getMin());
-        Assertions.assertEquals(max, stats.getMax());
-        Assertions.assertEquals(average, stats.getAverage(), 0.0);
+        Assertions.assertEquals(15L, stats.getSum());
+        Assertions.assertEquals(1, stats.getMin());
+        Assertions.assertEquals(5, stats.getMax());
+        Assertions.assertEquals(3.0, stats.getAverage(), 0.0);
         Assertions.assertEquals(this.synchList.size(), stats.getCount());
     }
 }

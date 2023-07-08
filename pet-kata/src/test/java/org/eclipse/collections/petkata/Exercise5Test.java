f942f26e415460991bd7f738c45bd18c7cd19dde
Donald Raab <Donald.Raab@bnymellon.com>
2023-07-08 18:21:16 -0400

    Remove extra test from PetKata.
    Signed-off-by: Donald Raab <Donald.Raab@bnymellon.com>

--- pet-kata/src/test/java/org/eclipse/collections/petkata/Exercise5Test.java
+++ pet-kata/src/test/java/org/eclipse/collections/petkata/Exercise5Test.java
@@ -95,40 +95,4 @@ public class Exercise5Test extends PetDomainForKata
         Assertions.assertEquals("Harry Hamster", petOwner.getFullName());
         Assertions.assertEquals("🐹 🐹", petOwner.getPets().makeString(" "));
     }
-
-    @Test
-    @Tag("KATA")
-    public void refactorToEclipseCollections()
-    {
-        Assertions.fail("Refactor to Eclipse Collections");
-
-        // Replace List and ArrayList with Eclipse Collections types
-        var people = new ArrayList<Person>();
-        people.add(new Person("Mary", "Smith").addPet(PetType.CAT, "Tabby", 2));
-        people.add(new Person("Bob", "Smith")
-                .addPet(PetType.CAT, "Dolly", 3)
-                .addPet(PetType.DOG, "Spot", 2));
-        people.add(new Person("Ted", "Smith").addPet(PetType.DOG, "Spike", 4));
-        people.add(new Person("Jake", "Snake").addPet(PetType.SNAKE, "Serpy", 1));
-        people.add(new Person("Barry", "Bird").addPet(PetType.BIRD, "Tweety", 2));
-        people.add(new Person("Terry", "Turtle").addPet(PetType.TURTLE, "Speedy", 1));
-        people.add(new Person("Harry", "Hamster")
-                .addPet(PetType.HAMSTER, "Fuzzy", 1)
-                .addPet(PetType.HAMSTER, "Wuzzy", 1));
-        people.add(new Person("John", "Doe"));
-
-        // Replace Set and HashSet with Eclipse Collections types
-        var petAges = new HashSet<Integer>();
-        for (Person person : people)
-        {
-            for (Pet pet : person.getPets())
-            {
-                petAges.add(pet.getAge());
-            }
-        }
-
-        //extra bonus - convert to a primitive collection
-        var expectedSet = Sets.mutable.with(1, 2, 3, 4);
-        Assertions.assertEquals(expectedSet, petAges, "Extra Credit - convert to a primitive collection");
-    }
 }
